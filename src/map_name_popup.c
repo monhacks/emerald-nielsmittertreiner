b7e4c7536425f2fe2a1c9c193087e6d5293d429f
Niels Mittertreiner <mittertreinerniels@gmail.com>
2023-04-25 04:16:18 +0200

    Align time of day string to right

--- src/map_name_popup.c
+++ src/map_name_popup.c
@@ -152,7 +152,8 @@ static void ShowMapNamePopUpWindow(void)
     withoutPrefixPtr = &(mapDisplayHeader[3]);
     FormatDecimalTimeWithoutSeconds(withoutPrefixPtr, gSaveBlock2Ptr->inGameClock.hours, gSaveBlock2Ptr->inGameClock.minutes, gSaveBlock2Ptr->is24HClockMode);
 
-    AddTextPrinterParameterized(GetSecondaryPopUpWindowId(), FONT_SMALL, mapDisplayHeader, 200, 6, TEXT_SKIP_DRAW, NULL);
+    //GetStringRightAlignXOffset(FONT_SMALL, mapDisplayHeader, DISPLAY_WIDTH);
+    AddTextPrinterParameterized(GetSecondaryPopUpWindowId(), FONT_SMALL, mapDisplayHeader, GetStringRightAlignXOffset(FONT_SMALL, mapDisplayHeader, DISPLAY_WIDTH) - 8, 6, TEXT_SKIP_DRAW, NULL);
     CopyWindowToVram(GetPrimaryPopUpWindowId(), COPYWIN_FULL);
     CopyWindowToVram(GetSecondaryPopUpWindowId(), COPYWIN_FULL);
 }
